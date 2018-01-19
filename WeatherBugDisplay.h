674815e82a3ef62ababa7053f0c2050ef267e879
roleohibachi <roleohibachi@gmail.com>
2018-01-18 14:07:45 -1000

    Corrections

--- WeatherBugDisplay.h
+++ WeatherBugDisplay.h
@@ -30,16 +30,30 @@ class WeatherBugDisplay
   private:
   SoftwareSerial *_swSerial;
   HardwareSerial *_hwSerial;
-  uint8_t* _data[];
   size_t _xmit(const char str[]);
   size_t _xmit(char c);
   size_t _xmit(uint16_t n);
-  size_t _xmit(unsigned long n, int base);
-  size_t _xmit(unsigned long n, int base) ;
+  size_t _xmit(unsigned long n);
   size_t _xmitln(void) ;
   size_t _xmitln(const char c[]) ;
-  size_t _xmitln(char c) ;
-
-
+  size_t _xmitln(char c);
+  
+  uint16_t _hours;
+  uint16_t _minutes;
+  uint16_t _month;
+  uint16_t _day;
+  uint16_t _indtemp;
+  uint16_t _outtemp;
+  uint16_t _auxtemp;
+  uint16_t _humidity;
+  uint16_t _baropress;
+  uint16_t _dailyrain;
+  uint16_t _monthlyrain;
+  uint16_t _yearlyrain;
+  uint16_t _windvel;
+  uint16_t _winddir;
+  uint16_t _avgwindvel;
+  uint16_t _avgwinddir;
+  uint16_t _barorate;
 	
 #endif
